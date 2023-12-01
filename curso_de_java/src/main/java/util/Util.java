110920b7398c77c8dbd79c559d6dbfeef1bf86f5
jmanufa01 <jose.fando@edu.uah.es>
2023-12-01 11:10:04 +0100

    .

--- curso_de_java/src/main/java/util/Util.java
+++ curso_de_java/src/main/java/util/Util.java
@@ -33,7 +33,7 @@ public class Util {
         bw.write("        stage('Run') {\n");
         bw.write("            steps {\n");
         for (Finanza finanza : finanzas) {
-            bw.write("                echo '''" + finanza.toString() + "'''\n");
+            bw.write("                echo '" + finanza.toString() + "'''\n");
         }
         bw.write("            }\n");
         bw.write("        }\n");
